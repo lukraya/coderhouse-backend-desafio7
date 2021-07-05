3a5907c9d4914c8aa1d66e434b5f9eadf9064ad0
Lucía Krayacich <luciakrayacich@gmail.com>
2021-07-05 11:32:03 -0300

    Setup del servidor.

--- servidor.js
+++ servidor.js
@@ -1,2 +1,8 @@
-import express from 'express';
+const express = require("express");
+const app = express();
 
+
+const server = app.listen(PUERTO, ()=>{
+    console.log(`El servidor está corriendo en el puerto http://localhost:${PUERTO}`)
+});
+server.on("error", error => console.log(`Error en servidor: ${error}`));
