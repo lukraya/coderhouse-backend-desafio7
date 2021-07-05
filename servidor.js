9948dec18ee3da761c81adf1cd26571aab2823e4
Lucía Krayacich <luciakrayacich@gmail.com>
2021-07-05 12:31:15 -0300

    Desafio completo.

--- servidor.js
+++ servidor.js
@@ -1,7 +1,52 @@
 const express = require("express");
-const app = express();
+const fs = require("fs");
 
+const app = express();
 const PUERTO = 8080;
+
+/* const getProductos = async ()=>{
+    try {
+        const contenido = await fs.promises.readFile("./productos.txt");
+        return JSON.parse(contenido)
+    }
+    catch (error) {
+        console.log(`No se pudo leer el archivo. Error: ${error}`)
+    }
+}
+const productos = getProductos(); */
+const data = fs.readFileSync("./productos.txt");
+const productos = JSON.parse(data);
+let cantItems = 0;
+let cantItem = 0;
+
+app.get('/items', (req, res)=>{
+    cantItems++;
+    //console.log(productos);
+    const respuesta = {
+        items: productos,
+        cantidad: productos.length
+    };
+    res.json(respuesta)
+})
+
+app.get('/item-random', (req, res)=>{
+    cantItem++;
+    const n = Math.floor(Math.random() * productos.length);
+    const respuesta = {
+        item: productos[n]
+    };
+    res.json(respuesta)
+})
+
+app.get('/visitas', (req, res)=>{
+    const respuesta = {
+        visitas: {
+            items: cantItems,
+            item: cantItem
+        }
+    }
+    res.json(respuesta)
+})
 
 const server = app.listen(PUERTO, ()=>{
     console.log(`El servidor está corriendo en el puerto http://localhost:${PUERTO}`)
